e1085c8a897c5713d79d553aaad9b31e487363e3
rodrigoabam <rodrigoab.am@gmail.com>
2020-07-03 10:24:16 -0400

    modal

--- script.js
+++ script.js
@@ -3,43 +3,17 @@ const dishes = document.querySelectorAll(".dish")
 
 for(let dish of dishes) {
     dish.addEventListener("click", () => {
-        const dishId = dish.getAttribute("id")
-        const dish4 = dish.getElementsByTagName("h4")
-        
+        let img = dish.getAttribute("id")
+        let h4 = dish.querySelector("h4").innerText
+        let p = dish.querySelector("p").innerText
+                
         modalOverlay.classList.add("active")
-        console.log(dishId)
-        console.log(dish4)
-        modalOverlay.querySelector("img").src = `/imagens/${dishId}.png`
-        modalOverlay.querySelector("h4").innerHTML = dish4.innerHTML;
-        modalOverlay.querySelector("p").innerHTML = `${dishId}`
-       
+        modalOverlay.querySelector("img").src = `/imagens/${img}.png`
+        modalOverlay.querySelector("h4").innerHTML = `${h4}`
+        modalOverlay.querySelector("p").innerHTML = `${p}`
     })
 }
-
 
 document.querySelector(".close-modal").addEventListener("click", () => {
     modalOverlay.classList.remove("active")
 })
-
-
-/* [
-    {
-        id: dish.getAttribute("id")
-    },
-    {
-        imag: dish.querySelector("img")
-    },
-    {
-         h4: dish.querySelector("h4")
-    },
-    {
-        p: dish.querySelector("p")
-    }
-] */
-
-/* = {
-            id: dish.getAttribute("id"),
-            img: dish.querySelector("img"),
-            h4: dish.querySelector("h4"),
-            p: dish.querySelector("p")
-        } */
